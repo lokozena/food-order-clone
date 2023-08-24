7373f928b9727b7cbc7aab3b9022ffdcd98181a2
ibrahim özhan <87445324+lokozena@users.noreply.github.com>
2023-08-24 17:18:36 +0300

    food-order-clone

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+
+import "../../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>Alpinyo Food</h5>
+              <p>
+              Easily reach every good taste you have ever thought of
+              </p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Sunday - Thursday</span>
+                <p>10:00am - 11:00pm</p>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Friday</span>
+                <p>Off day</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p>Location: Bakirköy / İstanbul</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Phone: 09012345678</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Email: alpinyofood@gmail.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Newsletter</h5>
+            <p>Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Enter your email" />
+              <span>
+                <i class="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p className="copyright__text">
+              Copyright - 2023, 
+              
+            </p>
+          </Col>
+          
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
